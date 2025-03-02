cba70d26fbaa48a10f3514369d1110bd468f8b74
Zashhive <susanna.i.arnold@proton.me>
2025-03-02 16:24:43 +0900

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TantraMassageForCouples from './components/TantraMassageForCouples';
+import './index.css'; // Import Tailwind CSS
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Import FontAwesome
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<TantraMassageForCouples />);
