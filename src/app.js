85274a252e968fce84e6fd1f013117367769d1f7
Zashhive <susanna.i.arnold@proton.me>
2025-03-02 17:29:57 +0900

    Delete src directory

--- src/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import TantraMassageForCouples from './components/TantraMassageForCouples';
-import './index.css'; // Import Tailwind CSS
-import '@fortawesome/fontawesome-free/css/all.min.css'; // Import FontAwesome
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<TantraMassageForCouples />);
